d64b307c872468dbe81987f49acd5c58c218e4b5
Abel Thomas <abelathomas@gmail.com>
2020-09-27 09:08:15 -0700

    Added a auto reconnecter

--- cam_grabber.py
+++ cam_grabber.py
@@ -4,6 +4,8 @@ from time import sleep
 from datetime import datetime
 from os import path, mkdir, startfile
 
+print("----- Initialising Timeplase Script -----")
+
 # The Camera names/location and fixed image links
 UDN = "http://ns-webcams.its.sfu.ca/public/images/udn-current.jpg?nocache=1&update=15000&timeout=1800000&offset=4"
 AQPOND = "http://ns-webcams.its.sfu.ca/public/images/aqn-current.jpg?nocache=1&update=15000&timeout=1800000"
@@ -18,6 +20,7 @@ TRN = "http://ns-webcams.its.sfu.ca/public/images/towern-current.jpg?nocache=1&u
 active_cam = TFF
 active_name = "TFF"
 
+
 # Initialising grabber
 print("----- Initilising " + active_name + " Cam Grabber -----")
 counter = 0
@@ -29,21 +32,34 @@ while grabber_status:
     # Checking if the folder path exists
     folder_path = "./" + active_name + "/"
     if (not(path.exists(folder_path))):
-        # temp_path = "./" + active_name + "/0.jpeg"
-        # if (not(path.exists(temp_path))):
         mkdir(folder_path)
 
     # Grabbing the images from SFU website
     print("Grabbing " + active_name + " Image# " + str(counter))
     temp_img = requests.get(active_cam)
 
-    while True:
-        if (not(temp_img.content)):
+    while (not(temp_img.content)):
+        if (grabber_status):
             print("----- Requested " + active_name + " SFU cam is down -----")
-            sleep(5)
+
+            # If cam down, delay for grabber to reconnect
+            time_grab_reconnect = 5
+            time_last_grab = 0
+            while (time_last_grab <= time_grab_reconnect):
+                if keyboard.is_pressed('esc+q'):
+                    print("----- Terminating Cam Grabber Script -----")
+                    grabber_status = False
+                    break
+
+                time_last_grab += 0.1
+                sleep(0.1)
+
             temp_img = requests.get(active_cam)
         else:
             break
+
+    if (not(grabber_status)):
+        break
 
     # Dowloading images locally
     cur_time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
@@ -57,7 +73,6 @@ while grabber_status:
 
     # Waiting for the next grab or until keyboard interrupt
     time_last_grab = 0
-
     while (time_last_grab <= time_grab_delay):
         if keyboard.is_pressed('esc+q'):
             print("----- Terminating Cam Grabber Script -----")
@@ -68,6 +83,17 @@ while grabber_status:
         sleep(0.1)
 
 
-# print("----- Initilising Vid Stitcher -----")
-# startfile("vid_stitch.bat")
-# print("----- Timeplase Vid Completed -----")
+while True:
+    user_inp = input("--> Start Timelapse Vid Stitcher?(Y/N):")
+    if ((user_inp == 'Y') | (user_inp == 'y')):
+        print("----- Initilising Vid Stitcher -----")
+        startfile("vid_stitch.bat")
+        print("----- Timeplase Vid Completed -----")
+        break
+
+    elif ((user_inp == 'N') | (user_inp == 'n')):
+        print("----- Terminating Timeplase Script -----")
+        break
+
+    else:
+        print("---> Incorrect input, Please Try Again")
